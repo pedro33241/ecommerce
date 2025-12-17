c62bf8e0cac5bdcf3b6de18d6a006172145677e6
Pedro <pedrojorgeosorio@gmail.com>
2025-12-17 13:51:46 +0000

    feat: working in product

--- src/components/mook/manualProduct.tsx
+++ src/components/mook/manualProduct.tsx
@@ -198,5 +198,247 @@ export const manualProducts = [
         srcImage: "/ferramentaManual/25.jpg",
         alt: "imagem do produto",
         price: 0
-    }
+    },
+    {
+            id: 26,
+            nameProduct: "Martelo Cofragem 1 Orelha Cabo Bimatéria 27mm Flux",
+            details: "Martelo de Cofragem Flux apropriado para montagem e desmontagem de cofragens. Com unha que lhe permite extrair pontas e pregos.",
+            srcImage: "/ferramentaManual/26.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 27,
+            nameProduct: "Martelo Unha Cabo Fibra 29mm Flux",
+            details: "Martelo de Unha Flux apropriado para montagem e desmontagem de cofragens. Com unha que lhe permite extrair pontas e pregos.",
+            srcImage: "/ferramentaManual/27.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 28,
+            nameProduct: "Martelo Cofragem Cabo Fibra 700Gr Flux",
+            details: "Martelo de Cofragem Flux apropriado para montagem e desmontagem de cofragens. Com unha que lhe permite extrair pontas e pregos.",
+            srcImage: "/ferramentaManual/28.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 29,
+            nameProduct: "Martelo Bola Cabo Madeira",
+            details: "Martelo de Bola Flux, com cabo em madeira e corpo em aço carbono temperado de alta qualidade.",
+            srcImage: "/ferramentaManual/29.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 30,
+            nameProduct: "Maço Borracha Branca",
+            details: "Maço de Borracha apropriado para aplicação de cerâmica. Não deixa marcas. A sua cabeça em borracha reduz as vibrações e desconforto sentidos pelo utilizador.",
+            srcImage: "/ferramentaManual/30.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 31,
+            nameProduct: "Picadeira Faz-Tudo Cabo Fibra 600Gr Flux",
+            details: "Picadeira Faz-Tudo 600gr Flux, com cabo em bimatéria e corpo em aço carbono temperado de alta qualidade, ideal para bater e picar paredes.",
+            srcImage: "/ferramentaManual/31.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 32,
+            nameProduct: "Machadão Cabo Fibra 3Kgs Flux",
+            details: "Machadão Cunha 3Kg Flux com cabo de fibra e lâmina em aço temperado ideal para corte de lenha e trabalhos florestais.",
+            srcImage: "/ferramentaManual/32.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 33,
+            nameProduct: "Pé Cabra",
+            details: "Pé de Cabra Flux, em aço carbono de alta resistência, ideal para arrancar pregos e utilizar como alavanca.",
+            srcImage: "/ferramentaManual/33.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 34,
+            nameProduct: "Talhadeira",
+            details: "Talhadeira com corpo em aço carbono temperado de alta qualidade, ideal para trabalhar em pedra.",
+            srcImage: "/ferramentaManual/34.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 35,
+            nameProduct: "Marreta Cabo Bimatéria",
+            details: "Marreta Flux, com cabo em bimatéria e corpo em aço carbono temperado de alta qualidade, ideal para utilização com cinzel e talhadeira.",
+            srcImage: "/ferramentaManual/35.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 36,
+            nameProduct: "Ponteiro Proteção",
+            details: "Ponteiro com Proteção Flux com corpo em aço carbono temperado de alta qualidade e proteção em bimatéria.",
+            srcImage: "/ferramentaManual/36.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 37,
+            nameProduct: "Escopro Proteção",
+            details: "Escopro com Proteção Flux com corpo em aço carbono temperado de alta qualidade e proteção em bimatéria.",
+            srcImage: "/ferramentaManual/37.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 38,
+            nameProduct: "Pá Alumínio Cabo Madeira Flux",
+            details: "Pá de Alumínio com Cabo de Madeira Flux fabricada em alumínio de alta qualidade ideal para trabalhos de construção.",
+            srcImage: "/ferramentaManual/38.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 39,
+            nameProduct: "Pá Quadrada Cabo Madeira Flux",
+            details: "Pá Quadrada com Cabo de Madeira Flux fabricada em aço temperado de alta qualidade, a partir de uma única peça com tratamento térmico ideal para trabalhos de construção.",
+            srcImage: "/ferramentaManual/39.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 40,
+            nameProduct: "Pá Bico Cabo Madeira Flux",
+            details: "Pá de Bico com Cabo de Madeira Flux fabricada em aço temperado de alta qualidade, a partir de uma única peça com tratamento térmico ideal para trabalhos de construção.",
+            srcImage: "/ferramentaManual/40.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 41,
+            nameProduct: "Pá Valar Com Cabo Madeira Flux",
+            details: "Pá de Valar com Cabo de Madeira Flux fabricada em aço temperado de alta qualidade, a partir de uma única peça com tratamento térmico ideal para trabalhos de abertura de valas.",
+            srcImage: "/ferramentaManual/41.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 42,
+            nameProduct: "Alicate de Rebitar Pro 10,5\" Flux",
+            details: "Alicate de rebitar profissional, ideal para fixação rápida e segura de rebites em trabalhos de montagem, manutenção e reparação.",
+            srcImage: "/ferramentaManual/42.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 43,
+            nameProduct: "Alicate Corte Lateral",
+            details: "Alicate indicado para corte preciso de fios e cabos elétricos em trabalhos de instalação, manutenção e reparação. Possui lâminas afiadas e resistentes.",
+            srcImage: "/ferramentaManual/43.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 44,
+            nameProduct: "Alicate Bomba Agua 10\" Flux",
+            details: "Alicate robusto e ajustável, ideal para segurar, apertar e soltar tubos, porcas e conexões de diferentes tamanhos.",
+            srcImage: "/ferramentaManual/44.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 45,
+            nameProduct: "Alicate de Pressão",
+            details: "Alicate indicado para fixação firme e segura de peças durante trabalhos de manutenção, montagem e reparação. Possui sistema de ajuste rápido e travamento eficiente.",
+            srcImage: "/ferramentaManual/45.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 46,
+            nameProduct: "Alicate de Rebitar 10,5\" Flux",
+            details: "Alicate de rebitar eficiente, ideal para fixação de rebites em trabalhos de montagem, manutenção e reparação.",
+            srcImage: "/ferramentaManual/46.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 47,
+            nameProduct: "Alicate Universal CRV",
+            details: "Alicate Universal Flux em cromo vanadium e com cabo bimatéria.",
+            srcImage: "/ferramentaManual/47.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 48,
+            nameProduct: "Alicate Vazador 225 mm Flux",
+            details: "Alicate vazador 225mm Flux com cabo bimatéria. 6 tamanhos de furos. Fácil de alterar o tamanho de furo.",
+            srcImage: "/ferramentaManual/48.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 49,
+            nameProduct: "Alicate Descarnar Fio 150 mm Flux CRV",
+            details: "Alicate de descarnar fio 150mm Flux em cromo vanadium e com cabo bimatéria.",
+            srcImage: "/ferramentaManual/49.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 50,
+            nameProduct: "Alicate Porta Eletrodos 300 A Flux",
+            details: "Alicate porta eletrodos Flux, fabricado em material de alta resistência é adequado para trabalhar com corrente até 300A.",
+            srcImage: "/ferramentaManual/50.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 51,
+            nameProduct: "Alicate Massa 300 A Flux",
+            details: "Alicate massa Flux, fabricado em material de alta resistência é adequado para trabalhar com corrente até 300A.",
+            srcImage: "/ferramentaManual/51.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 52,
+            nameProduct: "Alicate Corta Rebites 200mm Flux",
+            details: "Alicate corta rebites 200mm Flux com corpo em aço forjado de alta resistência e punhos em bimatéria.",
+            srcImage: "/ferramentaManual/52.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 53,
+            nameProduct: "Alicate Pontas Curvas",
+            details: "Alicate de pontas curvas Flux em cromo vanadium e com cabo bimatéria.",
+            srcImage: "/ferramentaManual/53.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 54,
+            nameProduct: "Marreta Com Cabo",
+            details: "Marreta Flux, com cabo de madeira e corpo em aço carbono temperado de alta qualidade, ideal para utilização com cinzel e talhadeira.",
+            srcImage: "/ferramentaManual/54.jpg",
+            alt: "imagem do produto",
+            price: 0
+        },
+        {
+            id: 55,
+            nameProduct: "Forquim Cascalho",
+            details: "Forquim para Cascalho Flux fabricado em aço temperado de alta qualidade, a partir de uma única peça com tratamento térmico ideal para trabalhos de construção.",
+            srcImage: "/ferramentaManual/55.jpg",
+            alt: "imagem do produto",
+            price: 0
+        }
+    
+
 ]
