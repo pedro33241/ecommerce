1478c6b6a9529c4dc85bdaea03e215f567067aa6
Pedro <pedrojorgeosorio@gmail.com>
2026-01-22 11:20:06 +0000

    feat:implementing new lamp products

--- src/components/mook/battery.tsx
+++ src/components/mook/battery.tsx
@@ -1,59 +1,284 @@
 export const batteryProducts = [
+    
     {
         id: 1,
-        nameProduct: "Bateria compacta de 18 V",
-        details: "Produto de compras",
-        srcImage: "/battery1.jpg",
-        alt: "imagem de battery",
-        price: 2000
-    }, {
+        nameProduct: "BATERIA GBA 12V 2.0AH - BOSCH",
+        details: "Bateria Bosch de 12V e 2.0Ah, ideal para ferramentas compactas, garantindo desempenho consistente, autonomia fiável e carregamento rápido. Desenvolvida com a qualidade Bosch, oferece durabilidade superior e compatibilidade perfeita com equipamentos da linha 12V.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/1.jpg",
+        alt: "imagem de BATERIA GBA 12V 2.0AH - BOSCH",
+        price: 10000,
+        marca: "BOSCH",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
         id: 2,
-        nameProduct: "Bateria  12 V",
-        details: "Produto de compras",
-        srcImage: "/battery2.jpg",
-        alt: "imagem de battery",
-        price: 2000
-    }, {
+        nameProduct: "BATERIA GBA 18V 2,0 AH - BOSCH",
+        details: "Bateria Bosch 18V 2.0Ah, leve e potente, ideal para trabalhos profissionais e contínuos. Proporciona excelente equilíbrio entre autonomia e peso, aumentando a produtividade no dia a dia.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/2.jpg",
+        alt: "imagem de BATERIA GBA 18V 2,0 AH - BOSCH",
+        price: 10000,
+        marca: "BOSCH",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
         id: 3,
-        nameProduct: "Bateria Varta D43 60Ah",
-        details: "Produto de compras",
-        srcImage: "/battery3.jpg",
-        alt: "imagem de battery",
-        price: 2000
-    }, {
+        nameProduct: "BATERIA 2X GBA 4.0AH + CARREGADOR GAL 18 - BOSCH",
+        details: "Kit completo Bosch com duas baterias 18V 4.0Ah e carregador rápido GAL 18. Solução ideal para profissionais que precisam de máxima autonomia, carregamento eficiente e desempenho constante em obra.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/3.jpg",
+        alt: "imagem de BATERIA 2X GBA 4.0AH + CARREGADOR GAL 18 - BOSCH",
+        price: 10000,
+        marca: "BOSCH",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
         id: 4,
-        nameProduct: "Pilha max AA2",
-        details: "Produto de compras",
-        srcImage: "/battery4.jpg",
-        alt: "imagem de battery",
-        price: 2000
-    }, {
+        nameProduct: "BATERIA MULTIMARCAS 18 V 5.0AH - BOSCH",
+        details: "Bateria Bosch Multimarcas 18V 5.0Ah, desenvolvida para aplicações exigentes, oferecendo elevada autonomia e potência prolongada. Ideal para quem procura rendimento máximo e menos paragens.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/4.jpg",
+        alt: "imagem de BATERIA MULTIMARCAS 18 V 5.0AH - BOSCH",
+        price: 10000,
+        marca: "BOSCH",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
         id: 5,
-        nameProduct: "Bateria 9V1 - Energizer MAX Pilhas",
-        details: "Produto de compras",
-        srcImage: "/battery5.jpg",
-        alt: "imagem de battery",
-        price: 2000
-    }, {
+        nameProduct: "CAIXA D 4 PACOTS - ENERGIZER CARREGAD MINI 2 PILHA AA 2000MAH",
+        details: "Pilhas recarregáveis Energizer AA 2000mAh, ideais para dispositivos de alto consumo. Economia, sustentabilidade e desempenho fiável em múltiplos ciclos de carga.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/5.jpg",
+        alt: "imagem de ENERGIZER AA 2000MAH",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
         id: 6,
-        nameProduct: "Energizer MAX - Pilhas D2",
-        details: "Produto de compras",
-        srcImage: "/battery6.jpg",
-        alt: "imagem de battery",
-        price: 2000
-    }, {
+        nameProduct: "CAIXA D 4 PACOTS - ENERGIZER CARREGAD MINI 2 PILHA AAA 700MAH",
+        details: "Pilhas recarregáveis Energizer AAA 700mAh, perfeitas para equipamentos eletrónicos do dia a dia, oferecendo longa vida útil e excelente custo-benefício.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/6.jpg",
+        alt: "imagem de ENERGIZER AAA 700MAH",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
         id: 7,
-        nameProduct: "Energizer A76",
-        details: "Produto de compras",
-        srcImage: "/battery7.jpg",
-        alt: "imagem de battery",
-        price: 2000
-    }, {
+        nameProduct: "CAIXA D 12 PACOTS - ENERGIZER LITHIUM 1.5V A76 BP2",
+        details: "Pilhas de lítio Energizer AA 1.5V, ideais para dispositivos de alto desempenho. Maior durabilidade, excelente performance em temperaturas extremas e longa vida útil.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/7.jpg",
+        alt: "imagem de ENERGIZER LITHIUM A76",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
         id: 8,
-        nameProduct: "Energizer CR2032",
-        details: "Produto de compras",
-        srcImage: "/battery8.jpg",
-        alt: "imagem de battery",
-        price: 2000
+        nameProduct: "CAIXA D 12 PACOTS - ENERGIZER LITHIUM 12V A27",
+        details: "Bateria Energizer Lithium A27 12V, indicada para controles remotos, alarmes e dispositivos eletrónicos especiais. Potência fiável e longa duração.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/8.jpg",
+        alt: "imagem de ENERGIZER LITHIUM A27",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 9,
+        nameProduct: "CAIXA D 12 PACOTS - ENERGIZER LITHIUM 3V ECR1220",
+        details: "Bateria de lítio Energizer CR1220 3V, ideal para relógios, calculadoras e pequenos dispositivos eletrónicos. Energia estável e duradoura.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/9.jpg",
+        alt: "imagem de ENERGIZER ECR1220",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 10,
+        nameProduct: "CAIXA D 12 PACOTS - ENERGIZER LITHIUM 3V ECR1620",
+        details: "Bateria Energizer CR1620 3V, desenvolvida para aplicações eletrónicas de precisão, garantindo segurança, fiabilidade e longa vida útil.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/10.jpg",
+        alt: "imagem de ENERGIZER ECR1620",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 11,
+        nameProduct: "CAIXA D 12 PACOTS - ENERGIZER LITHIUM 3V ECR2016",
+        details: "Bateria Energizer CR2016 3V, ideal para comandos, balanças e dispositivos compactos, com desempenho consistente e duradouro.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/11.jpg",
+        alt: "imagem de ENERGIZER ECR2016",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 12,
+        nameProduct: "CAIXA D 12 PACOTS - ENERGIZER LITHIUM 3V ECR2025 /2 UND",
+        details: "Pilhas botão Energizer CR2025 3V, embalagem dupla, perfeitas para dispositivos eletrónicos sensíveis, com elevada fiabilidade e segurança.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/12.jpg",
+        alt: "imagem de ENERGIZER ECR2025",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 13,
+        nameProduct: "CAIXA D 12 PACOTS - ENERGIZER LITHIUM 3V ECR2032",
+        details: "Bateria botão Energizer CR2032 3V, amplamente utilizada em dispositivos médicos, comandos e eletrónica de precisão. Longa duração garantida.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/13.jpg",
+        alt: "imagem de ENERGIZER ECR2032",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 14,
+        nameProduct: "CAIXA D 12 PACOTS - ENERGIZER MAX 9V 522BP1",
+        details: "Bateria Energizer Max 9V, ideal para detectores de fumo, equipamentos de segurança e dispositivos profissionais, oferecendo potência fiável e duradoura.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/14.jpg",
+        alt: "imagem de ENERGIZER MAX 9V",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 15,
+        nameProduct: "CAIXA D 20 PACOTS - ENERGIZER MAX AA BL 2UN E91BP2",
+        details: "Pilhas alcalinas Energizer Max AA, desempenho superior para uso diário e profissional, garantindo energia de longa duração.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/15.jpg",
+        alt: "imagem de ENERGIZER MAX AA",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 16,
+        nameProduct: "CAIXA D 12 PACOTS - ENERGIZER MAX AA BL 4+2UN E91BP6",
+        details: "Pilhas Energizer Max AA em embalagem económica, ideais para equipamentos de uso frequente, com excelente rendimento e fiabilidade.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/16.jpg",
+        alt: "imagem de ENERGIZER MAX AA 4+2",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 17,
+        nameProduct: "CAIXA D 20 PACOTS - ENERGIZER MAX AA BL 4UN E91BP4",
+        details: "Pilhas Energizer Max AA, combinação perfeita de potência e durabilidade para aplicações domésticas e profissionais.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/17.jpg",
+        alt: "imagem de ENERGIZER MAX AA 4UN",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 18,
+        nameProduct: "CAIXA D 20 PACOTS - ENERGIZER MAX AAA BL 2UN E92BP2",
+        details: "Pilhas alcalinas Energizer Max AAA, ideais para dispositivos eletrónicos compactos, garantindo energia duradoura e consistente.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/18.jpg",
+        alt: "imagem de ENERGIZER MAX AAA 2UN",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 19,
+        nameProduct: "CAIXA D 12 PACOTS - ENERGIZER MAX AAA BL 4+2UN E92BP6",
+        details: "Pilhas Energizer Max AAA em formato económico, ideais para alto consumo com desempenho confiável e prolongado.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/19.jpg",
+        alt: "imagem de ENERGIZER MAX AAA 4+2",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 20,
+        nameProduct: "CAIXA D 20 PACOTS - ENERGIZER MAX AAA BL 4UN E92BP4",
+        details: "Solução eficiente para quem precisa de energia confiável em grande volume, com a qualidade reconhecida da Energizer.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/20.jpg",
+        alt: "imagem de ENERGIZER MAX AAA 4UN",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 21,
+        nameProduct: "CAIXA D 6 PACOTS - ENERGIZER MAX LR14C BL 2UN",
+        details: "Pilhas Energizer C (LR14), indicadas para lanternas, rádios e equipamentos de maior consumo energético.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/21.jpg",
+        alt: "imagem de ENERGIZER LR14C",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 22,
+        nameProduct: "CAIXA D 6 PACOTS - ENERGIZER MAX LR20D BL 2UN E95BP2",
+        details: "Pilhas Energizer D (LR20), ideais para equipamentos industriais e de longa duração, com potência constante e segura.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/22.jpg",
+        alt: "imagem de ENERGIZER LR20D",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 23,
+        nameProduct: "CAIXA D 12 PACOTS - ENERGIZER MAXPLUS AA 4UN EP91 BP4",
+        details: "Pilhas Energizer Max Plus AA, maior desempenho e durabilidade para dispositivos exigentes, garantindo eficiência superior.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/23.jpg",
+        alt: "imagem de ENERGIZER MAXPLUS AA",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 24,
+        nameProduct: "CAIXA D 12 PACOTS - ENERGIZER MAXPLUS AA BL 2UN X91RP2",
+        details: "Pilhas Energizer Max Plus AAA, ideais para eletrónicos do dia a dia, com desempenho melhorado e maior autonomia.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/24.jpg",
+        alt: "imagem de ENERGIZER MAXPLUS AA 2UN",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 25,
+        nameProduct: "CAIXA D 12 PACOTS - ENERGIZER MAXPLUS AAA 4UN EP92 BP4",
+        details: "Solução prática e económica para garantir energia duradoura em dispositivos eletrónicos compactos.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/25.jpg",
+        alt: "imagem de ENERGIZER MAXPLUS AAA",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 26,
+        nameProduct: "CAIXA D 12 PACOTS - ENERGIZER MAXPLUS AAA BL 2UN",
+        details: "Pilhas Energizer Max Plus AA, desenvolvidas para alto desempenho, oferecendo maior duração e fiabilidade comprovada.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/26.jpg",
+        alt: "imagem de ENERGIZER MAXPLUS AAA 2UN",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 27,
+        nameProduct: "CAIXA D 6 PACOTS - ENERGIZER RECAR. AA BL 2UN NH15RP2",
+        details: "Pilhas recarregáveis Energizer AA, ideais para quem procura economia e sustentabilidade sem abrir mão da performance.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/27.jpg",
+        alt: "imagem de ENERGIZER RECARREGAVEL AA",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
+    },
+    {
+        id: 28,
+        nameProduct: "CAIXA D 6 PACOTS - ENERGIZER RECAR. AAA BL 2UN NH12RP2",
+        details: "Pilhas recarregáveis Energizer AAA, perfeitas para uso contínuo em dispositivos eletrónicos, com longa vida útil.\nColoque no carrinho para receber a cotação deste artigo.",
+        srcImage: "/battery/28.jpg",
+        alt: "imagem de ENERGIZER RECARREGAVEL AAA",
+        price: 10000,
+        marca: "ENERGIZER",
+        garantia: "1 ano de GARANTIA sobre defeito de fabricação"
     }
+
 ]
